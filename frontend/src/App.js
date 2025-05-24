0d29ab6b51596dc2f8e70db4370478efbe79e181
Pranathi Meruva <pranathi.meruva03@gmail.com>
2025-05-23 21:28:56 -0400

    db creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Heading } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p={6} bg="gray.50" minH="100vh">
+      <Heading color="teal.500">Welcome to EchoJournal 🌿</Heading>
+    </Box>
   );
 }
 
